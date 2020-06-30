e2450e2b734df333d09887dadf04280a045d3379
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-06-30 13:57:05 +0200

    Basic project structure with navigation

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav" />
+    <TheSidenav :show="displaySideNav" @close="displaySideNav = false" />
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TheHeader from '@/components/Navigation/TheHeader.vue'
+import TheSidenav from '@/components/Navigation/TheSidenav.vue'
+
+export default Vue.extend({
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      displaySideNav: false,
+    }
+  },
+})
+</script>
+
+<style>
+html {
+  font-family: 'Noto Sans', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
